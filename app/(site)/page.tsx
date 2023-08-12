5d6b96b62d3715d5102dce52bb545d2a0211fab2
Evgeniy Pashko <evgpashko92@gmail.com>
2023-08-12 20:58:30 +0700

    listitem component

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
 
 export default function Home() {
     return (
@@ -7,10 +8,16 @@ export default function Home() {
                 <div className="mb-2">
                     <h1 className="text-white text-3xl font-semibold">Welcome back</h1>
                     <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
-                        List item
+                        <ListItem name="Liked Songs" image="/images/liked.png" href="liked" />
                     </div>
                 </div>
             </Header>
+            <div className="mt-2 mb-7 px-6">
+                <div className="flex justify-between items-center">
+                    <h2 className="text-white text-2xl font-semibold">Newest songs</h2>
+                </div>
+                <p>List of songs!</p>
+            </div>
         </div>
     );
 }
