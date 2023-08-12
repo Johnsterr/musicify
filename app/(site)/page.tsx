5a9b4da777b4260914d18385d4dd14587bcd21cb
Evgeniy Pashko <evgpashko92@gmail.com>
2023-08-12 20:13:23 +0700

    header inside page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,16 @@
-export default function Home() {
-    return <div className="text-green-500">Hello!</div>;
-}
+import Header from "@/components/Header";
 
+export default function Home() {
+    return (
+        <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+            <Header>
+                <div className="mb-2">
+                    <h1 className="text-white text-3xl font-semibold">Welcome back</h1>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
+                        List item
+                    </div>
+                </div>
+            </Header>
+        </div>
+    );
+}
