e89d91875bbdf7baab206db9d07255e14f6fa789
Evgeniy Pashko <evgpashko92@gmail.com>
2023-08-16 23:51:19 +0700

    ModalProvider

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { ProductWithPrice } from "@/types";
+
+interface ModalProviderProps {
+    products: ProductWithPrice[];
+}
+
+const ModalProvider: React.FC<ModalProviderProps> = ({ products }) => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
+
+    return (
+        <>
+            {/* ModalProvider */}
+            <div>ModalProvider</div>
+        </>
+    );
+};
+
+export default ModalProvider;
